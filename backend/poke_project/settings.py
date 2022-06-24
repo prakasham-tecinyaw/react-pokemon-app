23404a4dda5ddd63d409ea7da3ddfd858c2d1770
Prakasham Pillay <prakashamtechinyaw@gmail.com>
2022-06-24 22:32:03 +0800

    removed keys.

--- backend/poke_project/settings.py
+++ backend/poke_project/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^1*3ze=$k^f!o=a4bi75m_$$f!a9n6kfyt=kum#6rwkh1h0w+r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -105,8 +104,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'samprostreet@gmail.com'
-EMAIL_HOST_PASSWORD = 'aiidkrcxqlzbpxum'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
